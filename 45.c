0dbf69f5402987dd5303dbeb0b1dca748addf9fd
anbudhanush5 <35687834+anbudhanush5@users.noreply.github.com>
2018-02-19 14:22:41 +0530

    Create 45.c

---
+++ 45.c
@@ -0,0 +1,14 @@
+ #include<stdio.h>
+ #include<conio.h>
+            void main()
+            {
+            	int n=4897,count=0,s;
+            	while(n>0)
+            	{
+            		s=n%10;
+            		n=n/10;
+            		count++;
+            	}
+            	getch();
+            }
+     
